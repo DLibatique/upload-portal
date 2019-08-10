37a38c1ff63951ac5e85075c63a1d7f0927c8f2c
Justin Chiou <chiou.kai@gmail.com>
2019-08-10 15:23:19 -0400

    init function to deploy

---
+++ functions/src/config/gdrive.ts
@@ -0,0 +1,6 @@
+import { google } from "googleapis";
+
+export const drive = google.drive({
+  version: "v3",
+  auth: "AIzaSyC7_2Shw5o71DFdmCcSuA8CopQ5oaZk1rs"
+});
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,32 @@
-import * as functions from 'firebase-functions';
+import { storage } from "firebase-functions";
+import { drive } from "./config/gdrive";
+import * as fs from "fs";
 
-// Start writing Firebase Functions
-// https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+export const onFileUpload = storage.object().onFinalize(async object => {
+  try {
+    const folderId = "1dQSHZFvTzvneGrtvRrBHEyPBfJs9Ozmb";
+    const fileMetadata = {
+      name: object.name,
+      parents: [folderId]
+    };
+
+    const media = {
+      mimeType: object.contentType,
+      body: fs.createReadStream('files/photo.jpg')
+    };
+
+    console.log(`File uploaded: ${object.name}`);
+
+    const res = await drive.files.create({
+      requestBody: fileMetadata,
+      media: media,
+      fields: 'id'
+    });
+
+    console.log(res.data);
+    return res.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+});

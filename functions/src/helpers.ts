3af7b9641103c49153c3131421ae3e08961ba201
Justin Chiou <chiou.kai@gmail.com>
2019-08-10 19:23:55 -0400

    refax out functions pertaining to separate ops

---
+++ functions/src/helpers.ts
@@ -0,0 +1,56 @@
+import { storage } from "firebase-functions";
+import * as admin from "firebase-admin";
+import { drive } from "./config/gdrive";
+import * as https from "https";
+import * as fs from "fs";
+
+export async function getDownloadURL(bucket: string, name: string) {
+  try {
+    const file_url = await admin
+      .storage()
+      .bucket(bucket)
+      .file(name)
+      .getSignedUrl({
+        action: "read",
+        expires: Date.now() + 900000 // add 15 min from now
+      });
+    const fileURL: string = file_url[0];
+    return fileURL;
+  } catch (error) {
+    console.error(`Error getting the Signed URL: ${error}`);
+    return error;
+  }
+}
+
+export async function uploadToGoogleDrive(
+  o: storage.ObjectMetadata,
+  fId: string,
+  fURL: string
+) {
+  try {
+    const fileMetadata = {
+      name: o.name,
+      parents: [fId]
+    };
+
+    const fileStream = https.get(fURL, res => {
+      return res.pipe(fs.createWriteStream(o.name!));
+    });
+
+    const media = {
+      mimeType: o.contentType,
+      body: fileStream
+    };
+
+    const response = await drive.files.create({
+      requestBody: fileMetadata,
+      media: media,
+      fields: 'id'
+    });
+
+    return response;
+  } catch (error) {
+    console.error(`Error uploading to gDrive: ${error}`);
+    return error;
+  }
+}
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,44 +1,15 @@
 import { storage } from "firebase-functions";
-import * as admin from "firebase-admin";
 import { initFirebase } from "./config/firebase";
 initFirebase();
-import { drive } from "./config/gdrive";
-import * as https from "https";
-import * as fs from "fs";
+import { getDownloadURL, uploadToGoogleDrive } from "./helpers";
 
 export const saveToGDrive = storage.object().onFinalize(async object => {
   try {
     const fileBucket = object.bucket;
     const fileName = object.name!;
-    console.log(`File name: ${fileName}`);
-    const file_url = await admin
-      .storage()
-      .bucket(fileBucket)
-      .file(fileName)
-      .getSignedUrl({
-        action: "read",
-        expires: 604800
-      });
-    const fileURL = file_url[0]
-    console.log(`File media link: ${fileURL}`);
+    const fileURL: string = await getDownloadURL(fileBucket, fileName);
     const folderId = "1dQSHZFvTzvneGrtvRrBHEyPBfJs9Ozmb";
-    const fileMetadata = {
-      name: fileName,
-      parents: [folderId]
-    };
-    const media = {
-      mimeType: object.contentType,
-      body: https.get(fileURL, res => res.pipe(fs.createWriteStream(fileName)))
-    };
-
-    console.log(`File uploaded to Google Drive: ${object.name}`);
-
-    const response = await drive.files.create({
-      requestBody: fileMetadata,
-      media: media,
-      fields: 'id'
-    });
-
+    const response = await uploadToGoogleDrive(object, folderId, fileURL);
     console.log(`Link in Google Drive: ${response.data.webViewLink}`);
     return response.data;
   } catch (error) {
